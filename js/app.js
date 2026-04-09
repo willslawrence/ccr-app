35e020385ff003f6715bb97c162b63d8be791803
William Lawrence <wlawrence@helicopter.com.sa>
2026-04-09 16:51:11 +0300

    Bump to v2.10.2

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
    CCR APP - MAIN ROUTER & FAB NAV
    ==================================== */
 
-const APP_VERSION = '2.10.1';
+const APP_VERSION = '2.10.2';
 
 // ====================================
 // LAZY SCRIPT LOADER
